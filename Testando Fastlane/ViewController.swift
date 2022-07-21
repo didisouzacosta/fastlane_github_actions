52e97329673155d45eafb78731d08d270ad1f725
Adriano Souza Costa <adrianosouzacostaios@gmail.com>
2022-07-20 21:20:00 -0300

    Initial Commit

---
+++ Testando Fastlane/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Testando Fastlane
+//
+//  Created by Adriano Souza Costa on 20/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
